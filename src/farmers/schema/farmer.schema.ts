112218c90a0b80e78ec43965ff658a9a85284c62
kalpesh rane <kalpeshbrane@gmail.com>
2023-05-27 18:06:58 +0530

    added farmer schema

---
+++ src/farmers/schema/farmer.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({
+  timestamps: true,
+})
+export class Farmers extends Document {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true, unique: true })
+  UniqueRegNumber: string;
+
+  @Prop({ required: true })
+  landOwned: string;
+
+  @Prop({ required: true })
+  city: string;
+
+  @Prop({ required: true })
+  state: string;
+
+  @Prop({ required: true, unique: true })
+  mobileNumber: string;
+}
+
+export const FarmerSchema = SchemaFactory.createForClass(Farmers);
